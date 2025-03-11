77ad7cd3d048a63ce950a33b6c669bd0aef9f4d0
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-11 13:31:00 -0700

    Add product LINK to ProductsList component, optimized whole page reloading

--- ecommerce/app/ProductsList.tsx
+++ ecommerce/app/ProductsList.tsx
@@ -1,15 +1,16 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { Product } from "./product-data";
 
 export default function ProductsList({ products }: { products: Product[] }) {
   return (
     <div>
       {products.map(product => (
-        <div key={product.id}>
+        <Link key={product.id} href="/product-detail">
           <Image src={'/' + product.imageUrl} alt="Product image" width={150} height={150} />
           <h2>{product.name}</h2>
           <p>${product.price}</p>
-        </div>
+        </Link>
       ))}
     </div>
   )
