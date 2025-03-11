f47efdac7dcd3d6097b0c6f1963a3aaceb969713
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-11 15:09:09 -0700

    Add 404 handling for non-existent product detail pages

--- ecommerce/app/products/[id]/page.tsx
+++ ecommerce/app/products/[id]/page.tsx
@@ -1,13 +1,19 @@
+import NotFoundPage from "@/app/not-found";
 import { products } from "@/app/product-data";
 
 export default function ProductDetailPage({ params }: { params: { id: string } }) {
   const product = products.find(p => p.id === params.id);
+
+  if (!product) {
+    return <NotFoundPage/>
+  }
+
   return (
     <>
-      <h1>{product!.name}</h1>
-      <p>{product!.price}</p>
+      <h1>{product.name}</h1>
+      <p>{product.price}</p>
       <h3>Description</h3>
-      <p>{product!.description}</p>
+      <p>{product.description}</p>
     </>
   );
 }
