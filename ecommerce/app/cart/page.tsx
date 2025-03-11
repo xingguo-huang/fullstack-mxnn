9b3d433cbccc455023dc7e4fa8eb0f349a724d44
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-11 15:00:10 -0700

    Enhance Cart and Product Detail pages with more product information

--- ecommerce/app/cart/page.tsx
+++ ecommerce/app/cart/page.tsx
@@ -1,3 +1,23 @@
+'use client';
+
+import { useState } from 'react';
+import { products } from '../product-data';
+import Link from 'next/link';
+
 export default function CartPage() {
-    return <h1>Shopping Cart</h1>
-  }
+  const [cartIds] = useState(['123', '345']);
+
+  const cartProducts = cartIds.map(id => products.find(p => p.id === id)!);
+
+  return (
+    <>
+    <h1>Shopping Cart</h1>
+    {cartProducts.map(product => (
+      <Link key={product.id} href={"/products/" + product.id}>
+        <h3>{product.name}</h3>
+        <p>${product.price}</p>
+      </Link>
+    ))}
+    </>
+  )
+}
--- ecommerce/app/products/[id]/page.tsx
+++ ecommerce/app/products/[id]/page.tsx
@@ -2,5 +2,12 @@ import { products } from "@/app/product-data";
 
 export default function ProductDetailPage({ params }: { params: { id: string } }) {
   const product = products.find(p => p.id === params.id);
-  return <h1>{product!.name}</h1>
+  return (
+    <>
+      <h1>{product!.name}</h1>
+      <p>{product!.price}</p>
+      <h3>Description</h3>
+      <p>{product!.description}</p>
+    </>
+  );
 }
