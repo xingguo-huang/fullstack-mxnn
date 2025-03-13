4d181ba3377d9cea0b5cb4fe073e4ebf9bc4aab6
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-13 16:44:26 -0700

    Add POST route handler to allow adding products to the user's cart

--- ecommerce/app/api/users/[id]/cart/route.ts
+++ ecommerce/app/api/users/[id]/cart/route.ts
@@ -35,3 +35,23 @@ export async function GET(request: NextRequest, { params }: { params: Params }) 
     }
   });
 }
+
+type CartBody = {
+  productId: string;
+}
+
+export async function POST(request: NextRequest, { params }: { params: Params }) {
+  const userId = params.id;
+  const body: CartBody = await request.json();
+  const productId = body.productId;
+
+  carts[userId] = carts[userId] ? carts[userId].concat(productId) : [productId];
+  const cartProducts = carts[userId].map(id => products.find(p => p.id === id));
+
+  return new Response(JSON.stringify(cartProducts), {
+    status: 201,
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+}
