4f202d84109f9c0e490e7fd9cdc803a16e47c266
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-13 15:40:51 -0700

    Creating a shopping cart endpoint

---
+++ ecommerce/app/api/users/[id]/cart/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest } from 'next/server';
+import { products } from '@/app/product-data';
+
+type ShoppingCart = Record<string, string[]>;
+
+const carts: ShoppingCart = {
+  '1': ['123', '234'],
+  '2': ['345', '456'],
+  '3': ['234'],
+}
+
+type Params = {
+  id: string;
+}
+
+export async function GET(request: NextRequest, { params }: { params: Params }) {
+  const userId = params.id;
+  const productIds = carts[userId];
+
+  if (productIds === undefined) {
+    return new Response(JSON.stringify([]), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+  const cartProducts = productIds.map(id => products.find(p => p.id === id));
+
+  return new Response(JSON.stringify(cartProducts), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
