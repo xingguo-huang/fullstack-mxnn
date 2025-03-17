0cc5a1b0d7530690bb042b3a3cb9e55347fc8932
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-17 16:56:19 -0700

    fetch user cart data from MongoDB, ensuring dynamic retrieval of product information based on user ID

--- ecommerce/app/api/users/[id]/cart/route.ts
+++ ecommerce/app/api/users/[id]/cart/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest } from 'next/server';
 import { products } from '@/app/product-data';
+import { connectToDb } from '@/app/api/db';
 
 type ShoppingCart = Record<string, string[]>;
 
@@ -14,10 +15,12 @@ type Params = {
 }
 
 export async function GET(request: NextRequest, { params }: { params: Params }) {
-  const userId = params.id;
-  const productIds = carts[userId];
+  const { db } = await connectToDb();
 
-  if (productIds === undefined) {
+  const userId = params.id;
+  const userCart = await db.collection('carts').findOne({ userId });
+
+  if (!userCart) {
     return new Response(JSON.stringify([]), {
       status: 200,
       headers: {
@@ -26,7 +29,8 @@ export async function GET(request: NextRequest, { params }: { params: Params }) 
     });
   }
 
-  const cartProducts = productIds.map(id => products.find(p => p.id === id));
+  const cartIds = userCart.cartIds;
+  const cartProducts = await db.collection('products').find({ id:{$in: cartIds} }).toArray();
 
   return new Response(JSON.stringify(cartProducts), {
     status: 200,
