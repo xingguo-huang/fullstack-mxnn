d6c6694c126cecaf8e96d258414126c6d22f48e7
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-17 16:43:49 -0700

    Refactor GET route to retrieve product data from MongoDB instead of static data

--- ecommerce/app/api/products/[id]/route.ts
+++ ecommerce/app/api/products/[id]/route.ts
@@ -1,14 +1,15 @@
 import { NextRequest } from 'next/server';
-import { products } from '@/app/product-data';
+import { connectToDb } from '../../db';
 
 type Params = {
   id: string;
 }
 
 export async function GET(request: NextRequest, { params }: { params: Params }) {
+  const { db } = await connectToDb();
   const productId = params.id;
 
-  const product = products.find(p => p.id === productId);
+  const product = await db.collection('products').findOne({ id: productId });
 
   if (!product) {
     return new Response('Product not found!', {
